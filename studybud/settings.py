c384b659c798a4f6bf4a8ff7ebeb879459197d64
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2023-08-17 15:29:19 +0100

    trying again

--- studybud/settings.py
+++ studybud/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@x0zc@@z=7nl9zur)j3z&qm1s3y2z4rz@*f)y&h50-2!m#t$+m'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', "False").lower() == ('true')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
 
 # Application definition
 
@@ -87,6 +88,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+database_url= os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url )
 
 
 # Password validation
@@ -129,7 +132,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR  /'static/'
+    BASE_DIR  /'static'
 ]
 
 MEDIA_ROOT = BASE_DIR /'static/images'
