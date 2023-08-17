f7b721a07a3d96e14133d2fc0a81fec0139e8b3c
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2023-08-17 18:27:48 +0100

    n,m ,

--- studybud/settings.py
+++ studybud/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL ='base.user'
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,7 +135,7 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     BASE_DIR  /'static'
 ]
-
+STATIC_ROOT = BASE_DIR / "static"
 MEDIA_ROOT = BASE_DIR /'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -143,3 +144,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
