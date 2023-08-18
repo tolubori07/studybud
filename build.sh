ddddb66f08df6198f03704e6c494d87d6921b11a
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2023-08-18 07:08:47 +0100

    dvsd

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 if [[ $CREATE_SUPERUSER ]];
 then
-  python world_champ_2022/manage.py createsuperuser --no-input
+  python manage.py createsuperuser --no-input
 fi
 python manage.py makemigrations 
 python manage.py migrate
