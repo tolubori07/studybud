eb14bc23b19f908551b73c36ea96232e313d8434
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2023-08-18 12:35:22 +0100

    urls changes to accept api in 404 error

--- studybud/urls.py
+++ studybud/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import handler404
 from base import views
 from studybud import settings
 from django.views.static import serve
+from base.views import custom_404
+
+handler404 = custom_404
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
