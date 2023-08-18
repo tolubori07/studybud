92da1175ab7fa218a4989a4d7a90d24ac191c68c
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2023-08-18 15:51:01 +0100

    s

--- studybud/settings.py
+++ studybud/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'static/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 STATICFILES_DIRS = [
     BASE_DIR  /'static'
@@ -146,4 +146,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
