8649fdd921bed14dbcd8d1a16383986523476b28
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2023-08-29 22:23:40 +0100

    changes to build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py createsuperuser --Tolubori07@gmail.com admin2 --password Toluwabori0307
+python manage.py createsuperuser --email Tolubori07@gmail.com  
 python manage.py makemigrations 
 python manage.py migrate
