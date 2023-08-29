93b82c4e7ed552cd9a93ea2b66a376766a32efc6
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2023-08-29 22:34:57 +0100

    c

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py createsuperuser --email Tolubori07@gmail.com  
+python manage.py collectstatic --no-input 
 python manage.py makemigrations 
 python manage.py migrate
+python manage.py createsuperuser --email Tolubori07@gmail.com 
